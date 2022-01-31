9bc2d16d0b136045603cc4e95f2deece41adc3dc
Hoàng Đại <daihoangnam3it@gmail.com>
2022-01-31 11:02:36 +0700

    fix custom css

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import helmet from 'helmet';
 import routes from './routes';
-
+import path from 'path';
 import swaggerUI from 'swagger-ui-express';
 import swaggerDocument from './docs/Swagger';
 
@@ -18,8 +18,8 @@ const PORT = 8000;
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
-// app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static('public'));
 app.get('/', (req, res) => {
     return res.redirect('/docs');
 });
