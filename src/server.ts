f5f7fff0ef16653ab37f9891e3a8f353f880be5d
Hoàng Đại <daihoangnam3it@gmail.com>
2022-01-31 10:22:10 +0700

    check custom css

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,11 @@ import connectDB from './config/db';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import helmet from 'helmet';
-
+import path from 'path';
 import routes from './routes';
 
 import swaggerUI from 'swagger-ui-express';
 import swaggerDocument from './docs/Swagger';
-import index from './validates';
 
 dotenv.config();
 
@@ -20,16 +19,20 @@ const PORT = 8000;
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     return res.redirect('/docs');
 });
-
+var cssOptions = {
+    customCssUrl: '/custom.css',
+    customSiteTitle: 'Poopcode APIs',
+};
 app.use('/api', routes);
 app.use(
     '/docs',
-    express.static('node_modules/swagger-ui-dist/', { index: false }),
+    // express.static('node_modules/swagger-ui-dist/', { index: false }),
     swaggerUI.serve,
-    swaggerUI.setup(swaggerDocument, { explorer: true })
+    swaggerUI.setup(swaggerDocument, cssOptions)
 );
 app.use(handleErrorMessage);
 app.listen(PORT, () => {
